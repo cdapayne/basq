0804f51df69d7f4cb9142e42b6deb11a8b09bc55
cdapayne <carlton.payne@hotmail.com>
2025-08-25 21:25:40 -0400

    update

---
+++ Basq/Basq/BasqApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasqApp.swift
+//  Basq
+//
+//  Created by PayneBrain on 8/25/25.
+//
+
+import SwiftUI
+
+@main
+struct BasqApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Basq/BasqTests/BasqTests.swift
@@ -0,0 +1,17 @@
+//
+//  BasqTests.swift
+//  BasqTests
+//
+//  Created by PayneBrain on 8/25/25.
+//
+
+import Testing
+@testable import Basq
+
+struct BasqTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
